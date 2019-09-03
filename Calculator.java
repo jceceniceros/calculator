c6383bc75311cd651b9fe8c11ca9a63edff06b49
Juan Carlos <jc.espinosa.ceniceros@gmail.com>
2019-09-02 20:45:49 -0500

    Codigo fuente de calculadora

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+class Calculator {
+
+    private float sum(float x, float y) {
+        float result = 0.0;
+        // TODO
+        return result;
+    }
+
+    private float mult(float x, float y) {
+        float result = 0.0;
+        // TODO
+        return result;
+    }
+
+    private float subs(float x, float y) {
+        float result = 0.0;
+        // TODO
+        return result;
+    }
+
+    private float mod(float x, float y) {
+        float result = 0.0;
+        // TODO
+        return result;
+    }
+
+    private float div(float x, float y) {
+        float result = 0.0;
+        // TODO
+        return result;
+    }
+
+    public void name(String[] args) {
+
+        float a = 0.0;
+        float b = 0.0;
+        
+        // TODO : Get args a, b
+        // a = 
+        // b = 
+
+        System.out.println("Numero 1: " + a);
+        System.out.println("Numero 2: " + b);
+        System.out.println("Division: " + this.div(a, b));
+        System.out.println("Suma: " + this.sum(a, b));
+        System.out.println("Multiplicacion: " + this.mult(a, b));
+        System.out.println("Modulo: " + this.mod(a, b));
+        System.out.println("Resta: " + this.subs(a, b));
+    }
+
+}
