71fb78d420397154f16c552bcef1d18a6f926d61
Juan Carlos <jc.espinosa.ceniceros@gmail.com>
2019-09-09 20:42:19 -0500

    Libreria matematica

--- Calculator.java
+++ Calculator.java
@@ -1,51 +1,22 @@
 class Calculator {
 
-    private float sum(float x, float y) {
-        float result = 0.0;
-        // TODO
-        return result;
-    }
+    public static void main(String[] args) {
 
-    private float mult(float x, float y) {
-        float result = 0.0;
-        // TODO
-        return result;
-    }
-
-    private float subs(float x, float y) {
-        float result = 0.0;
-        // TODO
-        return result;
-    }
-
-    private float mod(float x, float y) {
-        float result = 0.0;
-        // TODO
-        return result;
-    }
-
-    private float div(float x, float y) {
-        float result = 0.0;
-        // TODO
-        return result;
-    }
-
-    public void name(String[] args) {
-
-        float a = 0.0;
-        float b = 0.0;
+        double a = 0.0;
+        double b = 0.0;
         
         // TODO : Get args a, b
-        // a = 
-        // b = 
+        // a = Double.parseDouble(args[0]);
+        // b = Double.parseDouble(args[1])
 
         System.out.println("Numero 1: " + a);
         System.out.println("Numero 2: " + b);
-        System.out.println("Division: " + this.div(a, b));
-        System.out.println("Suma: " + this.sum(a, b));
-        System.out.println("Multiplicacion: " + this.mult(a, b));
-        System.out.println("Modulo: " + this.mod(a, b));
-        System.out.println("Resta: " + this.subs(a, b));
+        System.out.println("Division: " + Math.div(a, b));
+        System.out.println("Suma: " + Math.sum(a, b));
+        System.out.println("Multiplicacion: " + Math.mult(a, b));
+        System.out.println("Modulo: " + Math.mod(a, b));
+        System.out.println("Resta: " + Math.subs(a, b));
+
     }
 
 }
---
+++ Math.java
@@ -0,0 +1,32 @@
+class Math {
+
+    public static double sum(double x, double y) {
+        double result = 0.0;
+        // TODO
+        return result;
+    }
+
+    public static double mult(double x, double y) {
+        double result = 0.0;
+        // TODO
+        return result;
+    }
+
+    public static double subs(double x, double y) {
+        double result = 0.0;
+        // TODO
+        return result;
+    }
+
+    public static double mod(double x, double y) {
+        double result = 0.0;
+        // TODO
+        return result;
+    }
+
+    public static double div(double x, double y) {
+        double result = 0.0;
+        // TODO
+        return result;
+    }
+}
